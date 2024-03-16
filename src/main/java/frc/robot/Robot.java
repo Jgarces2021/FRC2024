17b887a50e7c19eaff5478afafb71ef5011b9ea7
Jhon <jgrecesbontin@schools.nyc.gov>
2024-03-16 14:13:26 -0400

    split robotvision out of robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,28 +4,13 @@
 
 package frc.robot;
 
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.CvSink;
-import edu.wpi.first.cscore.CvSource;
-import edu.wpi.first.cscore.MjpegServer;
-import edu.wpi.first.net.PortForwarder;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
-// import org.photonvision.PhotonCamera;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -37,26 +22,33 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
  * directory.
  */
 public class Robot extends TimedRobot {
-  private final CANSparkMax m_frontLeft = new CANSparkMax(2, MotorType.kBrushed);
-  private final CANSparkMax m_backLeft = new CANSparkMax(1, MotorType.kBrushed);
 
-  private final CANSparkMax m_frontRight = new CANSparkMax(3, MotorType.kBrushed);
-  private final CANSparkMax m_backRight = new CANSparkMax(4, MotorType.kBrushed);
+  private final CANSparkMax m_frontLeft;
+  private final CANSparkMax m_backLeft;
 
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_frontLeft::set, m_frontRight::set);
+  private final CANSparkMax m_frontRight;
+  private final CANSparkMax m_backRight;
+  private final DifferentialDrive m_robotDrive;
   private final XboxController m_controller = new XboxController(0);
   private final Timer m_timer = new Timer();
-  Thread m_visionThread;
-  Thread m_dataThread;
+  private final RobotVision vision;
 
   public Robot() {
+    m_frontLeft = new CANSparkMax(2, MotorType.kBrushed);
+    m_backLeft = new CANSparkMax(1, MotorType.kBrushed);
+    m_frontRight = new CANSparkMax(3, MotorType.kBrushed);
+    m_backRight = new CANSparkMax(4, MotorType.kBrushed);
+    m_backLeft.follow(m_frontLeft);
+    m_backRight.follow(m_frontRight);
+
+    m_robotDrive = new DifferentialDrive(m_frontLeft::set, m_frontRight::set);
     SendableRegistry.addChild(m_robotDrive, m_frontLeft);
     SendableRegistry.addChild(m_robotDrive, m_frontRight);
     SendableRegistry.addChild(m_robotDrive, m_backLeft);
     SendableRegistry.addChild(m_robotDrive, m_backRight);
 
-    m_backLeft.follow(m_frontLeft);
-    m_backRight.follow(m_frontRight);
+    vision = new RobotVision();
+    vision.startThreads();
   }
 
   /**
@@ -66,69 +58,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_dataThread = new Thread(() -> {
-      NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-      while (!Thread.interrupted()) {
-        double targetOffsetAngle_Horizontal = table.getDoubleTopic("tx").getEntry(0).get();
-        double targetOffsetAngle_Vertical = table.getDoubleTopic("ty").getEntry(0).get();
-        double targetArea = table.getDoubleTopic("ta").getEntry(0).get();
-        double targetSkew = table.getDoubleTopic("ts").getEntry(0)
-            .get();
-        System.out.println(
-            targetOffsetAngle_Horizontal + " " + targetOffsetAngle_Vertical + " " + targetArea + " " + targetSkew);
-      }
-    });
-    m_visionThread.setDaemon(true);
-
-    m_dataThread.start();
-
-    for (int port = 5800; port <= 5807; port++) {
-      PortForwarder.add(port, "limelight.local", port);
-    }
-
-    m_visionThread = new Thread(
-        () -> {
-          // Get the UsbCamera from CameraServer
-          MjpegServer camera = CameraServer.addServer("limelight.local", 5800);
-
-          // Set the resolution
-          camera.setResolution(640, 480);
-
-          // Get a CvSink. This will capture Mats from the camera
-          CvSink cvSink = CameraServer.getVideo();
-          // Setup a CvSource. This will send images back to the Dashboard
-          CvSource outputStream = CameraServer.putVideo("Rectangle", 640, 480);
-
-          // Mats are very memory expensive. Lets reuse this Mat.
-          Mat mat = new Mat();
-
-          // This cannot be 'true'. The program will never exit if it is. This
-          // lets the robot stop this thread when restarting robot code or
-          // deploying.
-          while (!Thread.interrupted()) {
-            // Tell the CvSink to grab a frame from the camera and put it
-            // in the source mat. If there is an error notify the output.
-            if (cvSink.grabFrame(mat) == 0) {
-              // Send the output the error.
-              outputStream.notifyError(cvSink.getError());
-              // skip the rest of the current iteration
-              continue;
-            }
-            // Put a rectangle on the image
-            Imgproc.rectangle(
-                mat, new Point(100, 100), new Point(400, 400), new Scalar(255, 255, 255), 5);
-            // Give the output stream a new image to display
-            outputStream.putFrame(mat);
-          }
-        });
-    m_visionThread.setDaemon(true);
-    m_visionThread.start();
-
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
     m_frontLeft.setInverted(true);
-    // m_frontRight.setInverted(true);
   }
 
   /** This function is run once each time the robot enters autonomous mode. */
@@ -163,30 +96,28 @@ public class Robot extends TimedRobot {
    * This function is called once each time the robot enters teleoperated mode.
    */
   @Override
-  public void teleopInit() {
-  }
+  public void teleopInit() {}
 
   /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getRightX());
+    m_robotDrive.arcadeDrive(
+      -m_controller.getLeftY(),
+      -m_controller.getRightX()
+    );
   }
 
   /** This function is called once each time the robot enters test mode. */
   @Override
-  public void testInit() {
-  }
+  public void testInit() {}
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {
-  }
+  public void testPeriodic() {}
 
   @Override
-  public void disabledPeriodic() {
-  }
+  public void disabledPeriodic() {}
 
   @Override
-  public void robotPeriodic() {
-  }
+  public void robotPeriodic() {}
 }
---
+++ src/main/java/frc/robot/RobotVision.java
@@ -0,0 +1,97 @@
+package frc.robot;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.MjpegServer;
+import edu.wpi.first.net.PortForwarder;
+import edu.wpi.first.networktables.DoubleEntry;
+import edu.wpi.first.networktables.DoubleTopic;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Timer;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+public class RobotVision {
+
+  private Thread m_visionThread;
+  private Thread m_dataThread;
+
+  public RobotVision() {
+    for (int port = 5800; port <= 5807; port++) {
+      PortForwarder.add(port, "limelight.local", port);
+    }
+    this.m_dataThread = this.initDataThread();
+    this.m_visionThread = this.initVisionThread();
+  }
+
+  private Thread initDataThread() {
+    Thread m_dataThread = new Thread(() -> {
+      NetworkTable table = NetworkTableInstance
+        .getDefault()
+        .getTable("limelight");
+      DoubleEntry targetArea = table.getDoubleTopic("ta").getEntry(0);
+      Timer m_Timer = new Timer();
+      while (!Thread.interrupted()) {
+        if (m_Timer.get() > 2) {
+          m_Timer.restart();
+          System.out.println(targetArea.get());
+        }
+      }
+    });
+    m_dataThread.setDaemon(true);
+    return m_dataThread;
+  }
+
+  private Thread initVisionThread() {
+    Thread m_visionThread = new Thread(() -> {
+      // Get the UsbCamera from CameraServer
+      MjpegServer camera = CameraServer.addServer("limelight.local", 5800);
+
+      // Set the resolution
+      camera.setResolution(640, 480);
+
+      // Get a CvSink. This will capture Mats from the camera
+      CvSink cvSink = CameraServer.getVideo();
+      // Setup a CvSource. This will send images back to the Dashboard
+      CvSource outputStream = CameraServer.putVideo("Rectangle", 640, 480);
+
+      // Mats are very memory expensive. Lets reuse this Mat.
+      Mat mat = new Mat();
+
+      // This cannot be 'true'. The program will never exit if it is. This
+      // lets the robot stop this thread when restarting robot code or
+      // deploying.
+      while (!Thread.interrupted()) {
+        // Tell the CvSink to grab a frame from the camera and put it
+        // in the source mat. If there is an error notify the output.
+        if (cvSink.grabFrame(mat) == 0) {
+          // Send the output the error.
+          outputStream.notifyError(cvSink.getError());
+          // skip the rest of the current iteration
+          continue;
+        }
+        // Put a rectangle on the image
+        Imgproc.rectangle(
+          mat,
+          new Point(100, 100),
+          new Point(400, 400),
+          new Scalar(255, 255, 255),
+          5
+        );
+        // Give the output stream a new image to display
+        outputStream.putFrame(mat);
+      }
+    });
+    m_visionThread.setDaemon(true);
+    return m_visionThread;
+  }
+
+  public void startThreads() {
+    m_dataThread.start();
+    m_visionThread.start();
+  }
+}
